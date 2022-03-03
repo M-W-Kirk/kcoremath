c880130967aa7a839e5c9c2308259d07f5570ee3
M-W-Kirk <micah@kirkpatrickcore.com>
2022-03-03 13:48:25 -0500

    updates added

--- kcoremath/kcoremath/settings.py
+++ kcoremath/kcoremath/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'website/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'website/static'),
+    os.path.join(BASE_DIR, 'static'),
    ]
