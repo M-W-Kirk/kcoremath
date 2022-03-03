17fb4ec4a28e25ed8157dc0a39add981d00c9e1b
M-W-Kirk <micah@kirkpatrickcore.com>
2022-03-03 13:05:01 -0500

    updates added

--- kcoremath/website/urls.py
+++ kcoremath/website/urls.py
@@ -4,12 +4,5 @@ from django.urls import path
 app_name = "website"
 
 urlpatterns = [
-    # ex:  https://www.optabot.ai/
-    path('', views.index, name='index'),
-    # ex:  https://www.optabot.ai/home/
-    path('home/', views.home, name='home'),
-    # ex:  https://www.optabot.ai/login/
-    path('login/', views.login, name='login'),
-    # ex:  https://www.optabot.ai/register/
-    path('register/', views.register, name='register'),
+   
 ]
