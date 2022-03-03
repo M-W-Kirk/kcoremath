78e4c887bd9f55a150d59b6ac9d4f517ec816aa9
M-W-Kirk <micah@kirkpatrickcore.com>
2022-03-03 15:55:18 -0500

    updates added

--- kcoremath/website/urls.py
+++ kcoremath/website/urls.py
@@ -5,4 +5,8 @@ app_name = "website"
 
 urlpatterns = [
    path('', views.home, name='home'),
+   path('add.html', views.add, name='add'),
+   path('subtract.html', views.subtract, name="subtract"),
+   path('multipy.html', views.multiply, name='multiply'),
+   path('divide.html', views.divide, name='divide'),
 ]
