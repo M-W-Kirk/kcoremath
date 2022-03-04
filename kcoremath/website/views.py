965b012eee32b6f2669bfa781b316fe94f07dbf5
M-W-Kirk <micah@kirkpatrickcore.com>
2022-03-03 20:08:33 -0500

    updates added

--- kcoremath/website/views.py
+++ kcoremath/website/views.py
@@ -5,11 +5,18 @@ def home(request):
     return render(request, 'home.html', {})
 
 def add(request):
+    from random import randint
+    num_1 = randint(0, 20)
+    num_2 = randint(0, 20)
+
     if request.method == "POST":
         answer = request.POST.get('answer')
         return render(request, 'add.html', {'answer': answer})
     
-    return render(request, 'add.html', {})
+    return render(request, 'add.html', {
+        'num_1': num_1,
+        'num_2': num_2,
+    })
 
 def subtract(request):
     return render(request, 'subtract.html', {})
