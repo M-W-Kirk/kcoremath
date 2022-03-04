111aa9fd5295e013f61a5f03d59872bd180b3ef9
M-W-Kirk <micah@kirkpatrickcore.com>
2022-03-03 22:08:49 -0500

    updates added

--- kcoremath/website/views.py
+++ kcoremath/website/views.py
@@ -17,14 +17,17 @@ def add(request):
 
         if int(answer) == correct_answer:
             my_answer = 'Correct!   ' + old_num_1 + ' + ' + old_num_2 + ' = ' + str(correct_answer) + '.'
+            color = 'success'
         else:
             my_answer = 'Incorrect!   ' + old_num_1 + ' + ' + old_num_2 + ' is not ' + answer + '.  It is ' + str(correct_answer) + '.'
+            color = 'danger'
 
         return render(request, 'add.html', context = {
             'answer':answer,
             'my_answer':my_answer,
             'num_1':num_1,
             'num_2':num_2,
+            'color':color,
             })
     
     return render(request, 'add.html', context = {
