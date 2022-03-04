0443e4d3cbae9e976bd4312c64c0786ba7cc26ec
M-W-Kirk <micah@kirkpatrickcore.com>
2022-03-04 00:00:12 -0500

    updates added

--- kcoremath/website/views.py
+++ kcoremath/website/views.py
@@ -107,4 +107,36 @@ def multiply(request):
         })
 
 def divide(request):
-    return render(request, 'divide.html', {})
+    num_1 = random.randint(a=0, b=10)
+    num_2 = random.randint(a=1, b=10)
+
+    if request.method == "POST":
+        answer = request.POST.get('answer')
+        old_num_1 = request.POST.get('old_num_1')
+        old_num_2 = request.POST.get('old_num_2')
+
+        correct_answer = int(old_num_1) * int(old_num_2)
+
+        if float(answer) == correct_answer:
+            outcome = 'Correct!   '
+            my_answer = old_num_1 + ' / ' + old_num_2 + ' = ' + str(correct_answer) + '.'
+            color = 'success'
+        else:
+            outcome = 'Incorrect!   '
+            my_answer = old_num_1 + ' / ' + old_num_2 + ' is not ' + answer + '.  It is ' + str(correct_answer) + '.'
+            color = 'danger'
+
+        return render(request, 'divide.html', context = {
+            'answer':answer,
+            'my_answer':my_answer,
+            'num_1':num_1,
+            'num_2':num_2,
+            'color':color,
+            'outcome':outcome,
+            })
+    
+    return render(request, 'divide.html', context = {
+        'num_1':num_1,
+        'num_2':num_2,
+        })
+    
