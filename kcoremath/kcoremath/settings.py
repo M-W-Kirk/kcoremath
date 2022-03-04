197cc7ed3685bfbe6833acdf1b68eeb3fd307f94
M-W-Kirk <micah@kirkpatrickcore.com>
2022-03-04 17:25:09 -0500

    updates added

--- kcoremath/kcoremath/settings.py
+++ kcoremath/kcoremath/settings.py
@@ -129,6 +129,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
    ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
