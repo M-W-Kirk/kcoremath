634180509f2e98a63236d181425f2c6b3fd48f4e
M-W-Kirk <micah@kirkpatrickcore.com>
2022-03-04 19:25:28 -0500

    updated file directory locations

--- kcoremath/kcoremath/wsgi.py
+++ kcoremath/kcoremath/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kcoremath.kcoremath.kcoremath.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kcoremath.settings')
 
 application = get_wsgi_application()
