179d6261ca7eb81b94871298fb487b341302bfd4
M-W-Kirk <micah@kirkpatrickcore.com>
2022-03-14 19:10:25 -0400

    initial models added

--- kcoremath/website/models.py
+++ kcoremath/website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Score(models.Model):
+    attempts = models.IntegerField()
+    correct = models.IntegerField()
+    incorrect = models.IntegerField()
+    date = models.DateField()
+    proficiency = models.DecimalField()
