4cb7ddd1d302a06d517979a541f92fbba5849c37
M-W-Kirk <micah@kirkpatrickcore.com>
2022-03-16 21:46:39 -0400

    updates added

--- kcoremath/website/models.py
+++ kcoremath/website/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Score(models.Model):
+    # look up the field arguments and delete this comment.
     user = models.OneToOneField()
     attempts = models.IntegerField()
     correct = models.IntegerField()
